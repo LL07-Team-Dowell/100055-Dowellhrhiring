7163228675bcf6cd04ec3e4fa34b242d43ced307
George Kibe <georgekibewambui@gmail.com>
2022-04-13 09:51:58 +0300

    configuring user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 # import jsonfield
 # Create your models here.
 
-class Profile(models.Model):
-    image = models.ImageField(null = True) #code to be commented
-    id = models.AutoField(primary_key = True)
-    JobTitle = models.CharField(max_length = 132,null = True)
-    position = models.CharField(max_length = 132,null = True)
-    user = models.ForeignKey(User , on_delete = models.CASCADE)
 
-    def __str__(self):
-        return str(self.id)
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    country = models.CharField(max_length=132, null=True)
+    bio = models.TextField(null=True)
+    # default="avatar.svg") to be uncommented after setting up a default image
+    avatar = models.ImageField(null=True)
+    JobTitle = models.CharField(max_length=132, null=True)
+    position = models.CharField(max_length=132, null=True)
+    is_hr = models.BooleanField(default=False, null=True)
+    is_team_leader = models.BooleanField(default=False, null=True)
 
-
+    def str(self):
+        return f'{self.username}, {self.email}'

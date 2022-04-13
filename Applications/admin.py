16b8354c64d6ea525650b073749b41b35c58548d
George Kibe <georgekibewambui@gmail.com>
2022-04-13 09:53:17 +0300

    removing unnecessary code

--- Applications/admin.py
+++ Applications/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import JobApplication, NewJobsApplication, HRCandidateStatus, TMCandidateStatus, AccountsCandidateStatus, InternetSpeed
+from .models import JobApplication, NewJobsApplication, HRCandidateStatus, TMCandidateStatus, AccountsCandidateStatus
 
 admin.site.register(JobApplication)
 admin.site.register(NewJobsApplication)
 admin.site.register(HRCandidateStatus)
 admin.site.register(AccountsCandidateStatus)
 admin.site.register(TMCandidateStatus)
-admin.site.register(InternetSpeed)

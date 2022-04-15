5d71afac24ba6f53be89836ecad53ae41545a466
George Kibe <georgekibewambui@gmail.com>
2022-04-15 08:43:57 +0300

    remving unnecessary code

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .Api import AddUser, UpdateStatus, MyTokenObtainPairView
+from .Api import AddUser, MyTokenObtainPairView
 #from .views import login_view, candidate_view
 
 urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/add_user', AddUser.as_view(), name='sign_up'),
-    path('api/update_status', UpdateStatus.as_view(), name='update_status'),
     #path('login/',  login_view, name = 'login_view'),
     #path('candidate_view/',  candidate_view, name = 'candidate_view'),
 ]

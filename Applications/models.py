63e2553b910ce918ab86101de4967ce18412b079
George Kibe <georgekibewambui@gmail.com>
2022-04-20 07:03:45 +0300

    modifying job and application models

--- Applications/models.py
+++ Applications/models.py
@@ -2,10 +2,22 @@ from django.db import models
 from accounts.models import User
 # Create your models here.
 
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=100)
+    project_leader = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.project_name, self.project_leader
+
+
 class Job(models.Model):
     title = models.CharField(max_length=100, null=False)
     description = models.TextField(null=False)
-    specific_terms = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     status = models.CharField(
         max_length=100, default="Not Receiving Applications")
     general_terms = models.JSONField(null=True)
@@ -13,6 +25,8 @@ class Job(models.Model):
     Technical_Specifications = models.JSONField()
     Payment_terms = models.JSONField()
     others = models.JSONField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -25,19 +39,18 @@ class JobApplication(models.Model):
     freelancePlatform = models.CharField(max_length=132, null=True)
     freelancePlatformUrl = models.URLField(null=True)
     country = models.CharField(max_length=132, null=True)
+    remarks_hr = models.CharField(max_length=500, null=True)
+    remarks_tl = models.CharField(max_length=500, null=True)
     status = models.CharField(max_length=132, null=True, default="Pending")
     others = models.JSONField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'Job Applications'
 
     def str(self):
         return f'{self.job}, {self.applicant}'
-
-
-class Project(models.Model):
-    project_name = models.CharField(max_length=100)
-    project_leader = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Meeting(models.Model):
@@ -50,3 +63,5 @@ class Meeting(models.Model):
     remarks = models.CharField(max_length=200)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     status = models.CharField(max_length=50, default="Pending")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)

e847f340de5b5a945c00dfe65661fc513c9459c4
George Kibe <georgekibewambui@gmail.com>
2022-04-20 08:42:23 +0300

    working on job applications and meetings apis

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,22 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import Task
-from .serializers import TaskSerializer
+from .models import User
+from .serializers import UserSerializer
 from rest_framework.decorators import api_view
 
 
 @api_view(['GET'])
-def tasks_view(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
 @api_view(['POST'])
-def add_new_task(request):
-    serializer = TaskSerializer(data=request.data)
+def register_user(request):
+    serializer = UserSerializer(data=request.data)
+    print(serializer)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)

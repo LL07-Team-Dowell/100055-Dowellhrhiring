73b8d93d8ee4bc30e5e7d25dbdeabdf561ff1ad7
George Kibe <georgekibewambui@gmail.com>
2022-04-20 07:35:31 +0300

    shwing freelancers and interns in the admin panel

--- Applications/admin.py
+++ Applications/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import JobApplication, Job, Meeting, Project
+from .models import JobApplication, Job, Meeting, Project, FreelancersAndInterns
 
 
 admin.site.register(Job)
 admin.site.register(JobApplication)
 admin.site.register(Project)
+admin.site.register(FreelancersAndInterns)
 admin.site.register(Meeting)

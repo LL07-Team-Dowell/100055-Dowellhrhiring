90f2ce80c4d844a7777e441cbfc2996ba8f3bea8
George Kibe <georgekibewambui@gmail.com>
2022-04-22 08:01:19 +0300

    adding the rehired table model

--- Applications/models.py
+++ Applications/models.py
@@ -93,3 +93,26 @@ class FreelancersAndInterns(models.Model):
 
     class Meta:
         verbose_name_plural = 'Freelancers And Interns'
+
+
+class RehiredCandidates(models.Model):
+    freelancer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="rehired_freelancers")
+    job_applied = models.ForeignKey(Job, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    tl_remarks = models.CharField(max_length=500, null=True)
+    CHOICES = (
+        ('Rehire', 'Rehire'),
+        ('Reject', 'Reject'),
+        ('Pay', 'Pay'),
+    )
+    teamlead_options = models.CharField(
+        max_length=70, choices=CHOICES, default="Rehire")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.freelancer.username}-{self.project}'
+
+    class Meta:
+        verbose_name_plural = 'Rehired Freelancers'

c67009b0da154c34e004dec947b281d4765da1bf
George Kibe <georgekibewambui@gmail.com>
2022-05-03 15:45:09 +0300

    adding relevant url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import get_users, RegisterView
+from .views import get_users, RegisterView, LoginView
 
 
 urlpatterns = [
     path('register_user/', RegisterView.as_view(), name='register_user'),
     path('get_users/', get_users, name='get_users'),
-    #path('login/', login, name='login'),
+    path('login_user/', LoginView.as_view(), name='login'),
 ]

123f0b9ce21c4ca33dece446aa22d28f49b83537
George Kibe <georgekibewambui@gmail.com>
2022-05-03 13:46:15 +0300

    revising api authentication view

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,13 @@ from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import UserSerializer
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+
+
+# create views here
+class RegisterView(APIView):
+    def post(self, request):
+        pass
 
 
 @api_view(['GET'])
@@ -11,13 +18,3 @@ def get_users(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
-
-
-@api_view(['POST'])
-def register_user(request):
-    serializer = UserSerializer(data=request.data)
-    print(serializer)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

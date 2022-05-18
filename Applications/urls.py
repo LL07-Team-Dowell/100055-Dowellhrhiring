f99fef90a4f237fe908be5b5f3d53ffaedfa3109
George Kibe <georgekibewambui@gmail.com>
2022-05-18 18:54:44 +0300

    testing projects' urls-get and post operations

--- Applications/urls.py
+++ Applications/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 
 from .views import application_view, get_jobs, add_new_job, get_applications, candindateview
 # from .views import UpdateaACStatus,
-from .views import hrview, update_job, delete_job, add_application
+from .views import hrview, update_job, delete_job, add_application, project
 
 urlpatterns = [
     path('get_jobs/', get_jobs, name='get_jobs'),
     path('add_job/', add_new_job, name='add_new_job'),
     path('update_job/', update_job, name="update_job"),
     path('delete_job/', delete_job, name="delete_job"),
+
+    path('project/', project, name="project"),
 
     path('get_applications/', get_applications, name="get_applications"),
     path('add_application/', add_application, name="add_application"),

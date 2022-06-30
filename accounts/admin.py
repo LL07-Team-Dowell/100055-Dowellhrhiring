3c433109228c6d1696937af262d786680d77d48b
George Kibe <georgekibewambui@gmail.com>
2022-06-30 14:13:09 +0300

    correcting errors in user and hr pages

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,24 @@
 from django.contrib import admin
-from .models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User
+
+# Register your models here.
 
 
 class UserAdmin(BaseUserAdmin):
     ordering = ["email"]
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = User
-    list_display = ["pkid", "id", "email", "username",
-                    "first_name", "last_name", "is_staff", "is_active"]
+    list_display = ['id', 'username', 'email', 'first_name',
+                    'last_name']
     list_display_links = ["id", "email"]
     list_filter = ["email", "username", "first_name",
                    "last_name", "is_staff", "is_active"]
 
+    search_fields = ["email", "username", "first_name", "last_name"]
 
-admin.site.register(User, UserAdmin)
+
+admin.site.register(User)

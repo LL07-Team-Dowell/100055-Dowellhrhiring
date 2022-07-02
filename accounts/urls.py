3617dc8619c999a5249a483ef457c5ce901ece8b
George Kibe <georgekibewambui@gmail.com>
2022-07-02 09:24:15 +0300

    correcting user view details

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path('register_user/', RegisterView.as_view(), name='register_user'),
     path('get_users/', get_users, name='get_users'),
     # path('login_user/', LoginView.as_view(), name='login'),
-    # path('user_view/', UserView.as_view(), name='user_view'),
+    path('user_view/', UserView.as_view(), name='user_view'),
     # path('logout_user/', LogoutView.as_view(), name='logout_user'),
     # # jwt urls
     # path('token/', jwt_views.TokenObtainPairView.as_view(),

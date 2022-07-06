b40eb0c834effa42fcc17d574475e26f59d91196
George Kibe <georgekibewambui@gmail.com>
2022-07-06 10:29:05 +0300

    adding dowell login function

--- Applications/views.py
+++ Applications/views.py
@@ -11,6 +11,26 @@ from .serializers import RejectedCandidateSerializer, TeamSerializer
 from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from django.shortcuts import redirect
+import requests
+
+
+def Dowell_Login(username, password):
+    url = "http://100014.pythonanywhere.com/api/login/"
+    userurl = "http://100014.pythonanywhere.com/api/user/"
+    payload = {
+        'username': username,
+        'password': password
+    }
+    with requests.Session() as s:
+        p = s.post(url, data=payload)
+        if "Username" in p.text:
+            return p.text
+        else:
+            user = s.get(userurl)
+            return user.text
+
+
+r = Dowell_Login()
 
 
 @api_view(['GET'])

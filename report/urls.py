2e4bdbaedf51e947b54fabe1a13fdd8b49cbb517
Manish Dowell <manish@dowellresearch.in>
2022-08-23 15:39:21 +0530

    Adding the report generation function to main project

--- report/urls.py
+++ report/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from report.views import home , candidate_count , main ,timeperiod
+from report.views import home , candidate_count , main ,timeperiod ,candidate_name
 from report.views import  hr_report, Teamlead_report, Candidate_report,account_report 
 
 urlpatterns =[
     path('home/',home, name= 'home'),
     path('timeperiod/',timeperiod, name= 'timeperiod'),
     path('main/',main, name= 'mainpage'),
+
     path('hr_report/',hr_report, name= 'hr_report'),
     path('Teamlead_report/',Teamlead_report, name= 'Teamlead_report'),
     path('Candidate_report/',Candidate_report, name= 'Candidate_report'),
     path('account_report/',account_report, name= 'account_report'),
     path('candidate_count/',candidate_count,name='candidate_count'),
+
+    path('candidate_name/',candidate_name,name='candidate_name'),
+
+
 ]
